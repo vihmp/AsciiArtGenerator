e217cc26ccef26447e39c5c7dcca71b21794e964
vihmp <vihmp.chel@mail.ru>
2019-06-22 22:45:50 +0500

    Add project files.

---
+++ AsciiArtGenerator/ImageConverter.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+
+namespace AsciiArtGenerator
+{
+    public partial class ImageConverter
+    {
+        public static char[,] ConvertImage(
+            Bitmap image, 
+            int beta,
+            double threshold,
+            ushort iterationsCount,
+            Action<int> ProgressUpdated)
+        {
+            int charNumHor = (int)Math.Round((double)image.Width / glyphWidth);
+            int charNumVert = (int)Math.Round((double)image.Height / glyphHeight);
+            int totalCharactersNumber = charNumVert * charNumHor;
+            int glyphSetSize = wNorm.GetLength(1);
+
+            double[,] v = SplitImage(image, charNumVert, charNumHor);
+
+            double[,] h = new double[glyphSetSize, totalCharactersNumber];
+            Random rand = new Random();
+
+            for (int i = 0; i < glyphSetSize; i++)
+            {
+                for (int j = 0; j < totalCharactersNumber; j++)
+                {
+                    h[i, j] = rand.NextDouble();
+                }
+            }
+
+            int progress = 0;
+            ushort step = (ushort)(iterationsCount / 10);
+
+            for (ushort i = 0; i < iterationsCount; i++)
+            {
+                UpdateH(v, wNorm, h, beta);
+
+                if((i + 1) % step == 0)
+                {
+                    progress += 10;
+
+                    if(progress < 100)
+                    {
+                        ProgressUpdated(progress);
+                    }
+                }
+            }
+
+            var result = GetAsciiRepresentation(h, charNumVert, charNumHor, threshold);
+            ProgressUpdated(100);
+
+            return result;
+        }
+
+        private static double[,] SplitImage(Bitmap image, int charNumVert, int charNumHor)
+        {
+            double[,] result =
+                new double[glyphHeight * glyphWidth, charNumHor * charNumVert];
+
+            for (int y = 0; y < charNumVert; y++)
+            {
+                for (int x = 0; x < charNumHor; x++)
+                {
+                    double[] bitmapVector = new double[glyphHeight * glyphWidth];
+
+                    for (int j = 0; j < glyphHeight; j++)
+                    {
+                        for (int i = 0; i < glyphWidth; i++)
+                        {
+                            byte color = (x * glyphWidth + i < image.Width) && (y * glyphHeight + j < image.Height) ?
+                                image.GetPixel(x * glyphWidth + i, y * glyphHeight + j).R : (byte)255;
+                            bitmapVector[glyphWidth * j + i] = color;
+                        }
+                    }
+
+                    double l2norm = Math.Sqrt(bitmapVector.Select(value => value * value).Sum());
+
+                    for (int k = 0; k < bitmapVector.Length; k++)
+                    {
+                        result[k, charNumHor * y + x] = bitmapVector[k] / l2norm;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static void UpdateH(double[,] v, double[,] w, double[,] h, int beta)
+        {
+            double[,] vApprox = MultiplyMatrix(w, h);
+
+            for (int j = 0; j < h.GetLength(0); j++)
+            {
+                for (int k = 0; k < h.GetLength(1); k++)
+                {
+                    double numerator = 0.0;
+                    double denominator = 0.0;
+
+                    for (int i = 0; i < w.GetLength(0); i++)
+                    {
+                        numerator += w[i, j] * v[i, k] / Math.Pow(vApprox[i, k], 2 - beta);
+                        denominator += w[i, j] * Math.Pow(vApprox[i, k], beta - 1);
+                    }
+
+                    h[j, k] = h[j, k] * numerator / denominator;
+                }
+            }
+        }
+
+        private static double[,] MultiplyMatrix(double[,] a, double[,] b)
+        {
+            double[,] c = new double[a.GetLength(0), b.GetLength(1)];
+
+            for (int i = 0; i < c.GetLength(0); i++)
+            {
+                for (int j = 0; j < c.GetLength(1); j++)
+                {
+                    for (int k = 0; k < a.GetLength(1); k++)
+                    {
+                        c[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+
+            return c;
+        }
+
+        private static char[,] GetAsciiRepresentation(
+            double[,] h, 
+            int charNumVert, 
+            int charNumHor, 
+            double threshold)
+        {
+            const int firstGlyphCode = 33;
+            char[,] result = new char[charNumVert, charNumHor];
+
+            for (int j = 0; j < h.GetLength(1); j++)
+            {
+                double max = 0.0;
+                int maxIndex = 0;
+
+                for (int i = 0; i < h.GetLength(0); i++)
+                {
+                    if (max < h[i, j])
+                    {
+                        max = h[i, j];
+                        maxIndex = i;
+                    }
+                }
+
+                result[j / charNumHor, j % charNumHor] =
+                   (max >= threshold) ? (char)(firstGlyphCode + maxIndex) : ' ';
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AsciiArtGenerator/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Globalization;
+
+namespace AsciiArtGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = args[0];
+            int beta;
+            double threshold;
+            ushort iterationsCount;
+
+            if (!int.TryParse(args[1], out beta))
+            {
+                Console.WriteLine("Error: 'beta' has invalid value, possible values are 0, 1 or 2");
+                return;
+            }
+
+            if((beta != 0) && (beta != 1) && (beta != 2))
+            {
+                Console.WriteLine("Error: 'beta' has invalid value, possible values are 0, 1 or 2");
+                return;
+            }
+
+            if(!double.TryParse(args[2], NumberStyles.Float, CultureInfo.InvariantCulture, out threshold))
+            {
+                Console.WriteLine(
+                    "Error: 'threshold' has invalid value, possible values are from 0.0 to 1.0");
+                return;
+            }
+
+            if((threshold < 0.0) || (threshold > 1.0))
+            {
+                Console.WriteLine(
+                    "Error: 'threshold' has invalid value, possible values are from 0.0 to 1.0");
+                return;
+            }
+
+            if(!ushort.TryParse(args[3], out iterationsCount))
+            {
+                Console.WriteLine(string.Format(
+                    "Error: 'iterations_count' has invalid value, possible values are from 1 to {0}",
+                    ushort.MaxValue));
+                return;
+            }
+
+            if(iterationsCount < 1)
+            {
+                Console.WriteLine(string.Format(
+                    "Error: 'iterations_count' has invalid value, possible values are from 1 to {0}",
+                    ushort.MaxValue));
+                return;
+            }
+
+            Bitmap image;
+            char[,] asciiRepresentation;
+
+            try
+            {
+                image = new Bitmap(inputFile);
+            }
+            catch
+            {
+                Console.WriteLine("Error: cannot open specified image");
+                return;
+            }
+
+            Console.WriteLine("Converting image...");
+
+            try
+            {
+                asciiRepresentation = ImageConverter.ConvertImage(
+                    image, 
+                    beta, 
+                    threshold, 
+                    iterationsCount,
+                    progress => Console.WriteLine(string.Format("{0}%", progress)));
+            }
+            catch
+            {
+                Console.WriteLine("Error: cannot convert specified image");
+                return;
+            }
+
+            Console.WriteLine("Saving result to output.html...");
+
+            using (FileStream output = File.Open("output.html", FileMode.Create, FileAccess.Write))
+            {
+                using (StreamWriter writer = new StreamWriter(output))
+                {
+                    writer.WriteLine("<font face=\"courier\">");
+                    for (int i = 0; i < asciiRepresentation.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < asciiRepresentation.GetLength(1); j++)
+                        {
+                            writer.Write(asciiRepresentation[i, j]);
+                        }
+                        writer.WriteLine("</br>");
+                    }
+                    writer.Write("</font>");
+                }
+            }
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
