b2729ce4d6b3e5a33c502bf0d289df392a4b2c24
vihmp <vihmp.chel@mail.ru>
2019-07-01 21:45:18 +0500

    updated command options

--- AsciiArtGenerator/Program.cs
+++ AsciiArtGenerator/Program.cs
@@ -5,55 +5,146 @@ using System.Globalization;
 
 namespace AsciiArtGenerator
 {
+    enum Modes
+    {
+        Pseudoinverse,
+        BetaDivergence
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Input file is undefined");
+                return;
+            }
+
+            if(args[0] == "/?")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Usage: AsciiArtGenerator <input_image> [-p] [-b <beta>] [-t <threshold>] ");
+                Console.WriteLine("       [-i <iterations_count>] [-o <output_file>]");
+                Console.WriteLine();
+                Console.WriteLine("Options:");
+                Console.WriteLine(string.Format("   {0,-25}{1}", "-p", "Use this option to convert the image using the"));
+                Console.WriteLine(string.Format("   {0,-25}{1}", string.Empty, "pseudoinverse."));
+                Console.WriteLine(string.Format("   {0,-25}{1}", "-b <beta>", "Sets 'beta' parameter, that enables the selection"));
+                Console.WriteLine(string.Format("   {0,-25}{1}", string.Empty, "of many cost functions (ignored if -p is set)."));
+                Console.WriteLine(string.Format("   {0,-25}{1}", string.Empty, "Possible values:"));
+                Console.WriteLine(string.Format("   {0,-25}{1}", string.Empty, "0 - Itakura-Saito Divergence"));
+                Console.WriteLine(string.Format("   {0,-25}{1}", string.Empty, "1 - Kullback-Leibler Divergence"));
+                Console.WriteLine(string.Format("   {0,-25}{1}", string.Empty, "2 - Squared Euclidean Distance"));
+                Console.WriteLine(string.Format("   {0,-25}{1}", "-t <threshold>", "Sets threshold for maximum activation values."));
+                Console.WriteLine(string.Format("   {0,-25}{1}", string.Empty, "Possible values are from 0.0 to 1.0."));
+                Console.WriteLine(string.Format("   {0,-25}{1}", "-i <iterations_count>", "Sets number of iterations of an algorithm."));
+                Console.WriteLine(string.Format("   {0,-25}{1} {2}", string.Empty, "Possible values are from 1 to", ushort.MaxValue));
+                Console.WriteLine(string.Format("   {0,-25}{1}", string.Empty, "(ignored if -p is set)."));
+                Console.WriteLine(string.Format("   {0,-25}{1}", "-o <output_file>", "Sets name of output HTML file."));
+                return;
+            }
+
             string inputFile = args[0];
-            int beta;
-            double threshold;
-            ushort iterationsCount;
+            Modes mode = Modes.BetaDivergence;
+            int beta = 1;
+            double threshold = 0.0;
+            ushort iterationsCount = 100;
+            string outputFile = "output.html";
 
-            if (!int.TryParse(args[1], out beta))
+            for(int i = 1; i < args.Length; i++)
             {
-                Console.WriteLine("Error: 'beta' has invalid value, possible values are 0, 1 or 2");
-                return;
-            }
+                switch(args[i])
+                {
+                    case "-p":
+                        mode = Modes.Pseudoinverse;
+                        break;
+                    case "-b":
+                        if((i + 2) > args.Length)
+                        {
+                            Console.WriteLine("Parameter -b is undefined");
+                            return;
+                        }
 
-            if((beta != 0) && (beta != 1) && (beta != 2))
-            {
-                Console.WriteLine("Error: 'beta' has invalid value, possible values are 0, 1 or 2");
-                return;
-            }
+                        if (!int.TryParse(args[i + 1], out beta))
+                        {
+                            Console.WriteLine(
+                                "Parameter -b has invalid value, possible values are 0, 1 or 2");
+                            return;
+                        }
 
-            if(!double.TryParse(args[2], NumberStyles.Float, CultureInfo.InvariantCulture, out threshold))
-            {
-                Console.WriteLine(
-                    "Error: 'threshold' has invalid value, possible values are from 0.0 to 1.0");
-                return;
-            }
+                        if ((beta != 0) && (beta != 1) && (beta != 2))
+                        {
+                            Console.WriteLine(
+                                "Parameter -b has invalid value, possible values are 0, 1 or 2");
+                            return;
+                        }
+                        i++;
+                        break;
+                    case "-t":
+                        if ((i + 2) > args.Length)
+                        {
+                            Console.WriteLine("Parameter -t is undefined");
+                            return;
+                        }
 
-            if((threshold < 0.0) || (threshold > 1.0))
-            {
-                Console.WriteLine(
-                    "Error: 'threshold' has invalid value, possible values are from 0.0 to 1.0");
-                return;
-            }
+                        if (!double.TryParse(
+                            args[i + 1], 
+                            NumberStyles.Float, 
+                            CultureInfo.InvariantCulture, 
+                            out threshold))
+                        {
+                            Console.WriteLine(
+                                "Parameter -t has invalid value, possible values are from 0.0 to 1.0");
+                            return;
+                        }
 
-            if(!ushort.TryParse(args[3], out iterationsCount))
-            {
-                Console.WriteLine(string.Format(
-                    "Error: 'iterations_count' has invalid value, possible values are from 1 to {0}",
-                    ushort.MaxValue));
-                return;
-            }
+                        if ((threshold < 0.0) || (threshold > 1.0))
+                        {
+                            Console.WriteLine(
+                                "Parameter -t has invalid value, possible values are from 0.0 to 1.0");
+                            return;
+                        }
+                        i++;
+                        break;
+                    case "-i":
+                        if ((i + 2) > args.Length)
+                        {
+                            Console.WriteLine("Parameter -i is undefined");
+                            return;
+                        }
 
-            if(iterationsCount < 1)
-            {
-                Console.WriteLine(string.Format(
-                    "Error: 'iterations_count' has invalid value, possible values are from 1 to {0}",
-                    ushort.MaxValue));
-                return;
+                        if (!ushort.TryParse(args[i + 1], out iterationsCount))
+                        {
+                            Console.WriteLine(string.Format(
+                                "Parameter -i has invalid value, possible values are from 1 to {0}",
+                                ushort.MaxValue));
+                            return;
+                        }
+
+                        if (iterationsCount < 1)
+                        {
+                            Console.WriteLine(string.Format(
+                                "Parameter -i has invalid value, possible values are from 1 to {0}",
+                                ushort.MaxValue));
+                            return;
+                        }
+                        i++;
+                        break;
+                    case "-o":
+                        if ((i + 2) > args.Length)
+                        {
+                            Console.WriteLine("Parameter -o is undefined");
+                            return;
+                        }
+
+                        outputFile = string.Format("{0}.html", args[i + 1]);
+                        i++;
+                        break;
+                    default:
+                        Console.WriteLine(string.Format("Invalid parameter {0}", args[i]));
+                        return;
+                }
             }
 
             Bitmap image;
@@ -65,7 +156,7 @@ namespace AsciiArtGenerator
             }
             catch
             {
-                Console.WriteLine("Error: cannot open specified image");
+                Console.WriteLine(string.Format("Cannot open file {0}", inputFile));
                 return;
             }
 
@@ -73,22 +164,29 @@ namespace AsciiArtGenerator
 
             try
             {
-                asciiRepresentation = ImageConverter.ConvertImage(
-                    image, 
-                    beta, 
-                    threshold, 
-                    iterationsCount,
-                    progress => Console.WriteLine(string.Format("{0}%", progress)));
+                if (mode == Modes.BetaDivergence)
+                {
+                    asciiRepresentation = ImageConverter.ConvertImage(
+                        image,
+                        beta,
+                        threshold,
+                        iterationsCount,
+                        progress => Console.WriteLine(string.Format("{0}%", progress)));
+                }
+                else
+                {
+                    asciiRepresentation = ImageConverter.ConvertImage(image, threshold);
+                }
             }
             catch
             {
-                Console.WriteLine("Error: cannot convert specified image");
+                Console.WriteLine("Cannot convert specified image");
                 return;
             }
 
-            Console.WriteLine("Saving result to output.html...");
+            Console.WriteLine(string.Format("Saving result to {0}...", outputFile));
 
-            using (FileStream output = File.Open("output.html", FileMode.Create, FileAccess.Write))
+            using (FileStream output = File.Open(outputFile, FileMode.Create, FileAccess.Write))
             {
                 using (StreamWriter writer = new StreamWriter(output))
                 {
